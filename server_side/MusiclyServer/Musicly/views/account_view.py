d7acb3b9c48dd45b98ce410cf12496ae15c23d1b
pabello <waclawiak.pawel@wp.pl>
2021-03-16 21:37:48 +0100

    Merge with main

--- server_side/MusiclyServer/Musicly/views/account_view.py
+++ server_side/MusiclyServer/Musicly/views/account_view.py
@@ -1,21 +1,21 @@
-from django.shortcuts import get_object_or_404, get_list_or_404
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.response import Response
 from ..models import Account, PasswordResetToken
-from ..serializers import AccountSerializer, AccountDetailsSerializer, AccountLifecycleSerializer
+from ..serializers import AccountDetailsSerializer, AccountLifecycleSerializer
 from password_strength import PasswordPolicy
 from rest_framework.decorators import api_view, permission_classes
 
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 import django.utils.timezone as time
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-
+from django.db import DatabaseError
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.dispatch import receiver
 
 from rest_framework.authtoken.models import Token
 from random import getrandbits
+from hashlib import sha256
 
 
 password_policy = PasswordPolicy.from_names(
@@ -35,9 +35,10 @@ def _password_secure(password: str):
 @api_view(['POST'])
 @permission_classes([])
 def register(request):
+    server_address = 'http://127.0.0.1:8000'  # Could be imported from some django constant probably
     account_info = request.data
-    serializer = AccountLifecycleSerializer(data=account_info)
 
+    serializer = AccountLifecycleSerializer(data=account_info)
     if serializer.is_valid():
         account = Account.objects.create(username=account_info['username'],
                                          email=account_info['email'])
@@ -45,11 +46,40 @@ def register(request):
         try:
             account.save()
             token = {"token": Token.objects.get(user=account).key}
-        except ValueError:
-            pass
+        except DatabaseError:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        send_mail(
+            subject='Email confirmation',
+            message='This is an automated email confirmation message from Musicly.\n\n'
+                    'Go to the link below to confirm your email address for Musicly account.'
+                    f'{server_address}/api/confirmEmail/{account.id}/{sha256(account.email)}\n',
+            html_message=render_to_string('password_reset_mail.html', {'server_address': server_address,
+                                                                       'account_id': account.id,
+                                                                       'token': sha256(account.email)}),
+            from_email='"noreply@musicly.com" <noreply@musicly.com>',
+            recipient_list=[account.email],
+            fail_silently=False
+        )
         return Response(status=status.HTTP_201_CREATED, data=token)
     else:
         return Response(serializer.errors)
+
+
+@api_view(['PATCH'])
+@permission_classes([])
+def confirm_email(request, pk, token):
+    user = Account.objects.get(pk=pk)
+    confirmation_token = sha256(user.email)
+    if token == confirmation_token:
+        user.email_confirmed = True
+        try:
+            user.save()
+        except DatabaseError:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            data={'details': 'server error, could not confirm email.'})
+    else:
+        return Response(status=status.HTTP_403_FORBIDDEN, data={'details': 'incorrect token provided.'})
 
 
 @api_view(['POST'])
@@ -77,7 +107,7 @@ def create_reset_token(request):
 
     try:
         reset_token.save()
-    except Exception as e:
+    except DatabaseError:
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={'details': 'could not create a token.'})
 
     # TODO: implement possibility for password change when you are logged in
