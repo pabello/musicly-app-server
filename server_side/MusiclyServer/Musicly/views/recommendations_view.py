045f5992242ae5206000df4301a3c7d431a107d1
Paweł Wacławiak <waclawiak.pawel@wp.pl>
2021-03-16 23:19:45 +0100

    Recommendations (#4)
    * Added endpoints for registration and log-in. Working on password reset. Rearranged the data scrutures and decided to let django manage the tables.
    
    * Finished registration, login and password reset modules.
    
    * Added playlist functionality. Started working on user music
    
    * Implemented user_music funcitonalities
    
    * Implemented recommendations endpoints along with temporary mechanism for getting (dummy) recommendations
    
    * Branch cleanup

---
+++ server_side/MusiclyServer/Musicly/views/recommendations_view.py
@@ -0,0 +1,27 @@
+from ..models import Recording, Account, UserMusic
+from ..serializers import RecordingSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.paginator import Paginator
+
+
+def get_recommendation_list(user):
+    # TODO: implement an actual algorithm
+    user_music = UserMusic.objects.filter(account=user)
+    recommendations = Recording.objects.all().exclude(pk__in=user_music)[:20]
+    return recommendations
+
+
+@api_view(['GET'])
+def recommendation_list(request):
+    recommendations = get_recommendation_list(request.user)
+    serializer = RecordingSerializer(recommendations, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def next_recommendation(request):
+    recommendation = get_recommendation_list(request.user)[0]
+    serializer = RecordingSerializer(recommendation)
+    return Response(serializer.data)
